7e536217b813dd2b8c7ece2312ad8ce59af0da80
Cydney Pollard <sincerelycydney087@gmail.com>
2021-07-06 20:14:04 -0400

    fixed parallex photos

--- src/components/FEMparallax.js
+++ src/components/FEMparallax.js
@@ -16,30 +16,34 @@ function FemParallax() {
             <div className="card-wrapper">
 
             <div className="card">
-                    <img src={PastelTown} alt="girl by vending machine" className="card-overlay image"/>
+                <div className="card-overlay ">
+                    <img src={PastelTown} alt="girl by vending machine" className="image"/>
+                    </div>
                     <div className="btn-text">
                     <div className="header-subtitle2">join our community of 2,000 members &#9734;</div>
-
-                    <button type="button" className="btn btn-secondary btn-rounded" href="/">join the community</button>
+                    <button type="button" className="btn btn-secondary btn-rounded" href="/">join the community &#9734;</button>
                     </div>
                 </div>
 
                 <div className="card">
-                    <img src={PurpleTown} alt="girl in space" className="card-overlay image"/>
+                    <div className="card-overlay ">
+                        <img src={PurpleTown} alt="girl in space" className="image"/>
+                    </div>
                     <div className="btn-text">
                     <div className="header-subtitle2">spend your coins in our FEMBabe shop &#9734;</div>
-
-                    <button type="button" className="btn btn-secondary btn-rounded" href="/">view our merch</button>
+                    <button type="button" className="btn btn-secondary btn-rounded" href="/">view our merch &#9734;</button>
                     </div>
                     
                 </div>
 
 
                 <div className="card">
-                    <img src={GreenTown} alt="girl next to arcade" className="card-overlay image"/>
+                    <div className="card-overlay">
+                        <img src={GreenTown} alt="girl next to arcade" className="image"/>
+                    </div>
                     <div className="btn-text">
                     <div className="header-subtitle2">check out our latest blog post &#9734;</div>
-                    <button type="button" className="btn btn-secondary btn-rounded" href="/">read the blog</button>
+                    <button type="button" className="btn btn-secondary btn-rounded" href="/">read the blog &#9734;</button>
                     </div>
                 </div>
 
--- src/components/history.js
+++ src/components/history.js
@@ -9,7 +9,7 @@ function History() {
                     <div class="col-md-5 offset-md-1 mx-3 my-3">
                     
                     <div class="view overlay">
-                        <img src={GamerGirl} class="img-fluid" alt="girl-in-arcade" />
+                        <img src={GamerGirl} class="img-fluid2" alt="girl-in-arcade" />
                         <div class="mask rgba-white-slight">
                         </div>
                     </div>
@@ -18,10 +18,12 @@ function History() {
                     <div class="col-md-6 text-md-left ml-3 mt-3">
                     <h4 class="h4 mb-4 header-title2">a history lesson</h4>
                     <p class="font-weight-normal header-subtitle2">
-                    Cupcake ipsum dolor sit amet jelly halvah. Candy jelly beans cookie cotton candy. Gummies dessert dessert 
-                    I love. Sesame snaps toffee marshmallow tart chupa chups. I love topping I love lollipop. Pastry jujubes 
-                    marzipan icing. Dessert jujubes gingerbread pudding. Gummi bears topping carrot cake gummies. 
-                    Gummies cookie macaroon I love sugar plum gingerbread oat cake lemon drops carrot cake.
+                    Feminaku began in 2018 with a vision from our founder CeeBee. She fell deeply in love with anime
+                    since she was in elementary school with Pokemon and Dragonball Z, but didn't watch seriously
+                    until she got to high school. From there, she found peace and happiness in watching her favorite shows after school.
+                    She even found herself going to anime cons with her friends, however while going there and being in several communities
+                    of anime lovers, she found that there weren't many communitites with people who looked like her. She wanted to create
+                    a space where she could talk about anime with other black girls and have a community by us, for us. 
                     </p>
                     </div>
 
