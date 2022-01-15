a6cba609e5d9e329b02613ce98deff32000d763b
Cydney Pollard <sincerelycydney087@gmail.com>
2022-01-14 22:46:55 -0500

    edited prime page, added review section

--- src/components/navbar-pages/prime.js
+++ src/components/navbar-pages/prime.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import BlackAnimeGirl from '../images/blackanimegirlqueen.gif'
+import BlackAnimeGirl from '../images/blackanimegirlqueen.gif';
+import FreeBlackGirl from '../images/freeblackgirl.jpg';
+
 
 
 class PrimePage extends React.Component {
@@ -36,7 +38,7 @@ class PrimePage extends React.Component {
                     <section className="hero is-medium">
                     <div className="hero-body">
                         <div className='container'>
-                        <img src={BlackAnimeGirl} alt="" className="image-float" />
+                        <img src={BlackAnimeGirl} alt="gif-drawing-of-black-girl-in-anime-form" className="image-float" />
 
                             <h1 className='header-title-third'>
                             Membership Essentials
@@ -66,7 +68,35 @@ class PrimePage extends React.Component {
                     </div>
                     </section>
 
-                    
+                    <section className="hero is-medium">
+                    <div className="hero-body">
+                        <div className='container'>
+                            <img src={FreeBlackGirl} alt='black-girl-sitting-on-stairs' className='image-float-second' />
+                        
+                        <h2 className='header-subtitle-fourth'>
+                            My favorite thing about Feminaku is the community and the amount of time they put into
+                            making sure we all feel welcomed. The culture they've made for what a community should be is
+                            unlike any community I've been apart of online. Happy to be here, and can't for what will come next.
+                        </h2>
+                        <h2 className="header-subtitle-third">
+                            Traylor Standford, Ventura, California
+                        </h2>
+                        </div>
+                    </div>
+                    </section>
+
+                    <section className='hero is-medium' style={{backgroundColor: 'lightgrey'}}>
+                        <div className='hero-body'>
+                            <div className='container'>
+                                <h1 className='header-title-second'>
+                                    Ready to join the tribe?
+                                </h1>
+
+                                <button type="button" className="btn btn-secondary btn-rounded footerbutton" href="/pricing">
+                                Get Started</button>
+                            </div>
+                        </div>
+                    </section>
 
 
             </div>
