a2cd6a5e1e036018bc910f839f80cf2112534672
Cydney Pollard <sincerelycydney087@gmail.com>
2022-02-13 15:27:09 -0500

    Updates

---
+++ precache-manifest.554983e0726964fb64afbed3efe282cf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9c276b8218233982ed26143e6309b2",
+    "url": "/feminakubabes/index.html"
+  },
+  {
+    "revision": "ab51ef104c55cf50d2f4",
+    "url": "/feminakubabes/static/css/main.15fa01fc.chunk.css"
+  },
+  {
+    "revision": "d023aa87b8875f40aa30",
+    "url": "/feminakubabes/static/js/2.55ede80f.chunk.js"
+  },
+  {
+    "revision": "ab51ef104c55cf50d2f4",
+    "url": "/feminakubabes/static/js/main.4d91e99a.chunk.js"
+  },
+  {
+    "revision": "320712126a057505deea",
+    "url": "/feminakubabes/static/js/runtime-main.ec60f8b4.js"
+  },
+  {
+    "revision": "037de7fce1900be280d84398f19d2e47",
+    "url": "/feminakubabes/static/media/blackanimegirlqueen.037de7fc.gif"
+  },
+  {
+    "revision": "cf5880e4d62c35d43929590203388326",
+    "url": "/feminakubabes/static/media/blackgirlglasses.cf5880e4.jpg"
+  },
+  {
+    "revision": "45d42a3eb71eba5f287ea5baa4f91800",
+    "url": "/feminakubabes/static/media/blackpottergirls.45d42a3e.jpg"
+  },
+  {
+    "revision": "07e4ec8aae877afa0c10799c4e9b7f8f",
+    "url": "/feminakubabes/static/media/feminakuprice.07e4ec8a.png"
+  },
+  {
+    "revision": "3c7b2fbbb3af385e0a4a7a9ee18a24d0",
+    "url": "/feminakubabes/static/media/freeblackgirl.3c7b2fbb.jpg"
+  },
+  {
+    "revision": "78a777ebcbb51477442830bfa512fff6",
+    "url": "/feminakubabes/static/media/payments.78a777eb.png"
+  },
+  {
+    "revision": "ce943c9ac4f8669b89faee3d3991b318",
+    "url": "/feminakubabes/static/media/sailor-moon-happy.ce943c9a.gif"
+  }
+]);
